4a8948e70c0a92de27805e6bd7f3fb1f6f05de46
Omar Vieyra <68872653+omarviga@users.noreply.github.com>
2025-04-21 11:29:54 -0600

    ok

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -76,7 +76,7 @@ const Dashboard: React.FC<DashboardProps> = ({ recentAssets, recentClients }) =>
           <div className="flex justify-between items-center mb-4">
             <div className="flex items-center">
               <Calendar className="h-5 w-5 text-polar-600 mr-2" />
-              <h2 className="text-xl font-semibold">Próximos </h2>
+              <h2 className="text-xl font-semibold">Mantenimientos Progamados</h2>
             </div>
           </div>
           
