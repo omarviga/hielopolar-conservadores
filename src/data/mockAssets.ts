54c160e25ec7ba549bb9e173aa196d56816233cb
aztro <joseomarvieyra@gmail.com>
2025-04-21 13:20:22 -0600

    Upload Images

--- src/data/mockAssets.ts
+++ src/data/mockAssets.ts
@@ -27,7 +27,7 @@ export const mockAssets: Asset[] = [
     assignedTo: 'Pescados Norte',
     capacity: '100 bolsas (5kg)',
     temperatureRange: '-15°C a -18°C',
-    imageSrc: 'https://images.unsplash.com/photo-1596461010617-8549605ab3e3?ixlib=rb-4.0.3&auto=format&fit=crop&w=900&q=60',
+    imageSrc: 'public/lovable-uploads/FT_FCC-100-2.jpg=crop&w=900&q=60',
     coordinates: [-100.5233, 20.0033] // Tarandacuao
   },
   {
@@ -39,7 +39,7 @@ export const mockAssets: Asset[] = [
     lastMaintenance: '10/01/2023',
     capacity: '40 bolsas (5kg)',
     temperatureRange: '-20°C a -25°C',
-    imageSrc: 'https://images.unsplash.com/photo-1589096044321-9274646f36fa?ixlib=rb-4.0.3&auto=format&fit=crop&w=900&q=60',
+    imageSrc: 'public/lovable-uploads/FT_FCC-40-2.jpg=crop&w=900&q=60',
     coordinates: [-100.4414, 19.8742] // Maravatío
   },
   {
