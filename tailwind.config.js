99bbbb03988ece077167f96727d3676670d1dfbc
Omar Vieyra <68872653+omarviga@users.noreply.github.com>
2025-05-04 11:45:11 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -15,63 +14,16 @@ export default {
         border: "hsl(var(--border))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-        sidebar: {
-          DEFAULT: "hsl(var(--sidebar-background))",
-          foreground: "hsl(var(--sidebar-foreground))",
-          primary: "hsl(var(--sidebar-primary))",
-          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
-          accent: "hsl(var(--sidebar-accent))",
-          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
-          border: "hsl(var(--sidebar-border))",
-          ring: "hsl(var(--sidebar-ring))",
-        },
+        // Añade otros colores que necesites
       },
-      backgroundColor: {
-        sidebar: {
-          DEFAULT: "hsl(var(--sidebar-background))",
-          accent: "hsl(var(--sidebar-accent))",
-        }
-      },
-      textColor: {
-        sidebar: {
-          DEFAULT: "hsl(var(--sidebar-foreground))",
-          foreground: "hsl(var(--sidebar-foreground))",
-          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
-        }
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
+      backgroundColor: ({ theme }) => ({
+        ...theme('colors'),
+        background: theme('colors.background')
+      }),
+      borderColor: ({ theme }) => ({
+        ...theme('colors'),
+        border: theme('colors.border')
+      }),
     },
   },
   plugins: [],
